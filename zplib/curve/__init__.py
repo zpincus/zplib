349bd912c7139ca89c8fb086963115aeec52bc8d
unknown <willie.zhang@gmail.com>
2015-05-11 10:26:53 -0500

    Added some docstrings for use with IPython.

---
+++ zplib/curve/__init__.py
@@ -0,0 +1,7 @@
+'''
+Curve
+-----
+Functions for computations over plane curves, approximated as series of points (polylines) or parametric splines.
+ - curve.geometry: basic algorithms for polyline curves.
+ - curve.interpolate: methods for resampling polylines and fitting them to smoothing splines (using scipy.interpolate.fitpack).
+ '''
