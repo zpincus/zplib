932a025c6150c17b5272ea37e13f8d8693aac7b3
Zach Pincus <zpincus@gmail.com>
2018-05-09 16:10:51 -0500

    bugfix to active_contour

--- zplib/image/active_contour.py
+++ zplib/image/active_contour.py
@@ -800,10 +800,11 @@ class StoppingCondition:
         self.max_iterations = max_iterations
         self.sample_every = sample_every
         self.changed_min = changed_min
+        self.cycle_max = cycle_max
         self.reset()
 
     def reset(self):
-        self.recent_changes = collections.deque(maxlen=cycle_max-1)
+        self.recent_changes = collections.deque(maxlen=self.cycle_max-1)
         self.i = 0
 
     def should_continue(self):
